07f2e93f63754ff4f6349cdab33aa1ca25b23714
eugeniosegala <eugenio.segala@hotmail.it>
2019-12-04 15:22:51 +0000

    Deploying to gh-pages from master adfac67dde287b0458e879712739d6d8ca817251

---
+++ precache-manifest.fdabfe8b0bd1f2e28cbd3a65db4d3d7d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fcbdb698bf186f37170d17c3e80ddd",
+    "url": "/devs-quiz/index.html"
+  },
+  {
+    "revision": "80d84c8ed15179e25f12",
+    "url": "/devs-quiz/static/css/2.27fdde94.chunk.css"
+  },
+  {
+    "revision": "42cac94135f8752c596e",
+    "url": "/devs-quiz/static/css/main.d1213297.chunk.css"
+  },
+  {
+    "revision": "80d84c8ed15179e25f12",
+    "url": "/devs-quiz/static/js/2.8a53dbe5.chunk.js"
+  },
+  {
+    "revision": "42cac94135f8752c596e",
+    "url": "/devs-quiz/static/js/main.9f679b98.chunk.js"
+  },
+  {
+    "revision": "811cab8f06918b1b52e9",
+    "url": "/devs-quiz/static/js/runtime-main.fd4e3b8b.js"
+  },
+  {
+    "revision": "47c96ed0bee477e6be241ec750329736",
+    "url": "/devs-quiz/static/media/01dffwqedaa.47c96ed0.jpg"
+  },
+  {
+    "revision": "ac572fb00bfb5c0ea4c8cb6357a2589f",
+    "url": "/devs-quiz/static/media/03dfeqfsvdfasd.ac572fb0.jpg"
+  },
+  {
+    "revision": "8ec586d1b7efef20d8f2f574fc7e7c16",
+    "url": "/devs-quiz/static/media/05hfbhwwd.8ec586d1.jpg"
+  },
+  {
+    "revision": "6772a27b822217a50be9b033afba3ce1",
+    "url": "/devs-quiz/static/media/06gsdgsha.6772a27b.jpg"
+  },
+  {
+    "revision": "f3ea63381cb59e0ab2045b4def28436e",
+    "url": "/devs-quiz/static/media/07hbfjndsjksm.f3ea6338.jpg"
+  },
+  {
+    "revision": "53738796db4a6f258729de5eb0656fca",
+    "url": "/devs-quiz/static/media/11fjnerueer.53738796.jpg"
+  },
+  {
+    "revision": "d5e91a4fd9ce4bb48b7b5b28f86d4b5d",
+    "url": "/devs-quiz/static/media/13ewrewfs.d5e91a4f.jpg"
+  },
+  {
+    "revision": "48e17415708ff1e0e2119f0b99d8215b",
+    "url": "/devs-quiz/static/media/15tytyree.48e17415.jpg"
+  },
+  {
+    "revision": "b88830270f40256d6d2d05106828ac06",
+    "url": "/devs-quiz/static/media/17ndjdnstrew.b8883027.jpg"
+  },
+  {
+    "revision": "8748aeca8a4d564d97da524f7a52b0a2",
+    "url": "/devs-quiz/static/media/19fjrwew.8748aeca.jpg"
+  },
+  {
+    "revision": "9173444edb58a8dc5ee4f35e050ebb30",
+    "url": "/devs-quiz/static/media/21nwrdnsdsa.9173444e.jpg"
+  },
+  {
+    "revision": "27b3606d6bcd48e84322744b5e661152",
+    "url": "/devs-quiz/static/media/23yhbgfq.27b3606d.jpg"
+  },
+  {
+    "revision": "d0a68b7b312afe2e2a010d4cd4bfec5a",
+    "url": "/devs-quiz/static/media/25ytghjqq.d0a68b7b.jpg"
+  },
+  {
+    "revision": "206af4e5dc334731eb05b7876e612598",
+    "url": "/devs-quiz/static/media/27urtvbd.206af4e5.jpg"
+  },
+  {
+    "revision": "8650df4e9e245ed29bd6615890b97541",
+    "url": "/devs-quiz/static/media/29ufbewew.8650df4e.jpg"
+  },
+  {
+    "revision": "bdbf89fc0585cc4b80ce9aede9cb1083",
+    "url": "/devs-quiz/static/media/30rewewwq.bdbf89fc.jpg"
+  },
+  {
+    "revision": "78d5ad8f8a7de472a11f553a08744075",
+    "url": "/devs-quiz/static/media/31rurueetjpg.78d5ad8f.jpg"
+  },
+  {
+    "revision": "68007aebf4724c7754e3f9bda875cb91",
+    "url": "/devs-quiz/static/media/32iuytr.68007aeb.jpg"
+  },
+  {
+    "revision": "e0d4034b3f07b541c5988639b2421fe5",
+    "url": "/devs-quiz/static/media/35vdsawq.e0d4034b.jpg"
+  },
+  {
+    "revision": "ce3687d63634153e29b1e03ea87c8578",
+    "url": "/devs-quiz/static/media/36hghglk.ce3687d6.jpg"
+  },
+  {
+    "revision": "ffaac25840ac038dc13a22425d43a577",
+    "url": "/devs-quiz/static/media/39jfrnjr.ffaac258.jpg"
+  },
+  {
+    "revision": "c206b4c789fa6a4c54cba6d080c34215",
+    "url": "/devs-quiz/static/media/41gunrfr.c206b4c7.jpg"
+  },
+  {
+    "revision": "b3a13afc6464739ceadd61abd72076f9",
+    "url": "/devs-quiz/static/media/43ireieir.b3a13afc.jpg"
+  },
+  {
+    "revision": "40812dc43996e7617b41b96bdce772b8",
+    "url": "/devs-quiz/static/media/45gfkgtortr.40812dc4.jpg"
+  },
+  {
+    "revision": "9d64dd9f937db5ccd1031367f2e74a1b",
+    "url": "/devs-quiz/static/media/47ereirieu.9d64dd9f.jpg"
+  },
+  {
+    "revision": "23a719f548c89f28dfa34efc60e24430",
+    "url": "/devs-quiz/static/media/48utrtrer.23a719f5.jpg"
+  },
+  {
+    "revision": "8e742663c23f317fdc5eec92574f465e",
+    "url": "/devs-quiz/static/media/49irewew.8e742663.jpg"
+  },
+  {
+    "revision": "032ae47fc6df7caf69787fe980adeae8",
+    "url": "/devs-quiz/static/media/game-over.032ae47f.gif"
+  },
+  {
+    "revision": "51a800479474f34dabbdeb8a5b4b3471",
+    "url": "/devs-quiz/static/media/life.51a80047.png"
+  },
+  {
+    "revision": "128b57116dc144322450f6ee92f7c0f4",
+    "url": "/devs-quiz/static/media/win.128b5711.gif"
+  }
+]);
